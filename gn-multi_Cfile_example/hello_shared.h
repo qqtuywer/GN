b3cacbb61860631542159b4d19185cd15ae153db
WZN3SZH <Zheng.WANG5@cn.bosch.com>
2020-07-01 20:02:48 +0800

    multi c files

---
+++ gn-multi_Cfile_example/hello_shared.h
@@ -0,0 +1,10 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TOOLS_GN_EXAMPLE_HELLO_SHARED_H_
+#define TOOLS_GN_EXAMPLE_HELLO_SHARED_H_
+
+int GetSharedText();
+
+#endif  // TOOLS_GN_EXAMPLE_HELLO_SHARED_H_
---
+++ gn-multi_Cfile_example/out/build.ninja.d
@@ -0,0 +1,1 @@
+build.ninja: ../BUILD.gn ../build/BUILD.gn ../build/BUILDCONFIG.gn ../build/toolchain/BUILD.gn ./args.gn ../.gn
